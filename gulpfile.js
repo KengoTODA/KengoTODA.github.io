3033c99109e7ed24861a9ec5141406188b2c8f06
Kengo TODA <skypencil@gmail.com>
2015-10-18 18:59:31 +0800

    minify html to reduce network cost

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
 
-gulp.task('default', function () {
+gulp.task('default', ['minify-css', 'minify-html']);
+
+gulp.task('minify-css', function () {
   return gulp.src(['css/normalize.min.css', 'css/main.css'])
     .pipe(concat('all.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-html', function () {
+  return gulp.src('html/*.html')
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('.'));
 });
